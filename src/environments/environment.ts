741e6bb623ac70b73f30378dfd8b2ffcd32d5902
Mauricio <89495810+MBakai@users.noreply.github.com>
2025-08-15 12:59:12 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000/meshwork/api', // URL de tu backend NestJS
-    apiSocket: 'http://localhost:3000',
+    production: true,
+    apiUrl: 'https://meshwork-backend.onrender.com/meshwork/api', // URL de tu backend NestJS
+    apiSocket: 'https://meshwork-backend.onrender.com',
     recaptcha: {
       siteKey: 'TU_SITE_KEY'
     }
