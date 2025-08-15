84c714d091a927a40b5558676b4169376eb19b63
Mauricio <89495810+MBakai@users.noreply.github.com>
2025-08-15 13:55:11 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet } from '@angular/router';
+import { Event as RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'MeshWork';
+  isLoading = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (event instanceof NavigationStart) {
+        this.isLoading = true; 
+      }
+      if (event instanceof NavigationEnd || event instanceof NavigationError) {
+        this.isLoading = false; 
+      }
+    });
+  }
 }
